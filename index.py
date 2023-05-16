b6e6d989a81f5e09e31ab9d2ef02883d8cd4f7b7
shalev <admin@shalev.eu.org>
2023-05-16 17:35:35 +0100

    web

--- index.py
+++ index.py
@@ -1,9 +1,15 @@
 reader = open("file/updateThisFile", "r", encoding="utf-8")
 writer = open("github-viewer.tsv", "w", encoding="utf-8")
+# change this
 allowanceforemptyline = 6
-notfoundcount = 0
+linkname = "link"
+titlename = "title"
 
+
+# dont change this
+notfoundcount = 0
 runcounter = 0
+writer.write(f"{linkname}\t{titlename}\n")
 while True:
     line = reader.readline()
     index = 0
@@ -14,7 +20,7 @@ while True:
             notfoundcount = 0
         else:
             found = True
-            writer.write(f"{line[0:index]}\t{line[index+1:-1]}\n")
+            writer.write(f"{line[0:index]}\t{line[index +1:-1]}\n")
             break
     if found == False:
         if notfoundcount < allowanceforemptyline:
